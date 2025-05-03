8cea4e1c5298a2b18744acbda6e2bd7f0b12f625
DaeHan Jang <abouttt57@gmail.com>
2025-05-03 16:19:12 +0900

    Add concepts

--- AGMath/include/agmath/agmath.hpp
+++ AGMath/include/agmath/agmath.hpp
@@ -1,4 +1,5 @@
 #pragma once
 
+#include "concepts.hpp"
 #include "constants.hpp"
 
---
+++ AGMath/include/agmath/concepts.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <type_traits>
+
+namespace agm
+{
+	template<typename T>
+	concept Integral = std::is_integral_v<T>;
+
+	template<typename T>
+	concept Floating = std::is_floating_point_v<T>;
+
+	template<typename T>
+	concept Numeric = std::is_arithmetic_v<T>;
+}
+
