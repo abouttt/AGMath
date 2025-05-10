1b6c5fb7e115b98534ba3bb2b8574331ab690992
DaeHan Jang <abouttt57@gmail.com>
2025-05-10 15:34:40 +0900

    Add Vector2Int

--- AGMath/include/agmath/agmath.h
+++ AGMath/include/agmath/agmath.h
@@ -3,6 +3,7 @@
 #include "random.h"
 #include "utilities.h"
 #include "vector2.h"
+#include "vector2_int.h"
 #include "vector3.h"
 #include "vector4.h"
 #include "quaternion.h"
--- AGMath/main.cpp
+++ AGMath/main.cpp
@@ -7,7 +7,7 @@ using namespace agm;
 
 int main()
 {
-	auto matrix = Matrix4x4(Vector4(1, 2, 3, 4), Vector4(5, 6, 7, 8), Vector4(9, 10, 11, 12), Vector4(13, 14, 15, 16));
-	std::cout << matrix.Transpose().ToString();
+	auto v = Vector2Int::ONE;
+	cout << v.ToString();
 }
 
