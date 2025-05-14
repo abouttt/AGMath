2893cef1ffcd7489dfec244f6df8215927231b26
DaeHan Jang <abouttt57@gmail.com>
2025-05-14 17:39:45 +0900

    Refactor utility functions

--- AGMath/include/agmath/utilities.h
+++ AGMath/include/agmath/utilities.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <cmath>
-#include <concepts>
+#include <cstdint>
 
 namespace agm
 {
@@ -83,11 +83,6 @@ namespace agm
 
 	inline float Repeat(float value, float length)
 	{
-		if (length == 0.f)
-		{
-			return 0.f;
-		}
-
 		return Clamp(value - std::floor(value / length) * length, 0.f, length);
 	}
 
@@ -113,16 +108,19 @@ namespace agm
 		return Repeat(angle + 180.f, 360.f) - 180.f;
 	}
 
-	template<typename T, typename U>
-	inline constexpr T Lerp(T a, T b, U t)
+	inline float AngleBetween(float a, float b)
 	{
-		return (T)(a + (b - a) * Clamp01(t));
+		return Abs(WrapAngle(b - a));
 	}
 
-	template<typename T, typename U>
-	inline constexpr T LerpUnclamped(T a, T b, U t)
+	inline constexpr float Lerp(float a, float b, float t)
 	{
-		return (T)(a + (b - a) * t);
+		return a + (b - a) * Clamp01(t);
+	}
+
+	inline constexpr float LerpUnclamped(float a, float b, float t)
+	{
+		return a + (b - a) * t;
 	}
 
 	inline float LerpAngle(float a, float b, float t)
@@ -175,14 +173,8 @@ namespace agm
 		return to * t + from * (1.f - t);
 	}
 
-	template<std::integral T>
-	inline constexpr T NextPowerOfTwo(T value)
+	inline constexpr int32_t NextPowerOfTwo(int32_t value)
 	{
-		if (value <= 1)
-		{
-			return 1;
-		}
-
 		value--;
 		value |= value >> 1;
 		value |= value >> 2;
@@ -194,143 +186,26 @@ namespace agm
 		return value;
 	}
 
-	template<std::integral T>
-	inline constexpr T ClosestPowerOfTwo(T value)
+	inline constexpr int32_t ClosestPowerOfTwo(int32_t value)
 	{
-		if (value <= 0)
-		{
-			return 0;
-		}
-
-		T nextPower = NextPowerOfTwo(value);
-		T prevPower = nextPower >> 1;
+		int32_t nextPower = NextPowerOfTwo(value);
+		int32_t prevPower = nextPower >> 1;
 		return (value - prevPower < nextPower - value) ? prevPower : nextPower;
 	}
 
-	template<std::integral T>
-	inline constexpr bool IsPowerOfTwo(T value)
+	inline constexpr bool IsPowerOfTwo(int32_t value)
 	{
-		return (value > 0) && (value & (value - 1)) == 0;
+		return (value & (value - 1)) == 0;
 	}
 
-	inline float Pow(float base, float exponent)
+	inline constexpr bool IsEven(int32_t value)
 	{
-		return std::pow(base, exponent);
+		return (value & 1) == 0;
 	}
 
-	inline float Exp(float value)
+	inline constexpr bool IsOdd(int32_t value)
 	{
-		return std::exp(value);
-	}
-
-	inline float Sqrt(float value)
-	{
-		return std::sqrt(value);
-	}
-
-	inline float InvSqrt(float value)
-	{
-		return 1.f / std::sqrt(value);
-	}
-
-	inline float Sin(float rad)
-	{
-		return std::sin(rad);
-	}
-
-	inline float Cos(float rad)
-	{
-		return std::cos(rad);
-	}
-
-	inline float Tan(float rad)
-	{
-		return std::tan(rad);
-	}
-
-	inline float Asin(float value)
-	{
-		return std::asin(value);
-	}
-
-	inline float Acos(float value)
-	{
-		return std::acos(value);
-	}
-
-	inline float Atan(float value)
-	{
-		return std::atan(value);
-	}
-
-	inline float Atan2(float y, float x)
-	{
-		return std::atan2(y, x);
-	}
-
-	inline float Log(float value)
-	{
-		return std::log(value);
-	}
-
-	inline float Log(float value, float base)
-	{
-		return std::log(value) / std::log(base);
-	}
-
-	inline float Log10(float value)
-	{
-		return std::log10(value);
-	}
-
-	inline float Ceil(float value)
-	{
-		return std::ceil(value);
-	}
-
-	inline int32_t CeilToInt(float value)
-	{
-		return (int32_t)std::ceil(value);
-	}
-
-	inline float Floor(float value)
-	{
-		return std::floor(value);
-	}
-
-	inline int32_t FloorToInt(float value)
-	{
-		return (int32_t)std::floor(value);
-	}
-
-	inline float Round(float value)
-	{
-		return std::round(value);
-	}
-
-	inline int32_t RoundToInt(float value)
-	{
-		return (int32_t)std::round(value);
-	}
-
-	inline float GammaToLinearSpace(float value)
-	{
-		if (value <= 0.04045f)
-		{
-			return value / 12.92f;
-		}
-
-		return Pow((value + 0.055f) / 1.055f, 2.4f);
-	}
-
-	inline float LinearToGammaSpace(float value)
-	{
-		if (value <= 0.0031308f)
-		{
-			return value * 12.92f;
-		}
-
-		return 1.055f * Pow(value, 1.0f / 2.4f) - 0.055f;
+		return (value & 1) == 1;
 	}
 }
 
